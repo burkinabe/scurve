248a361262a1a2495551e124feb10a4bec7b570d
SAVADOGO Abdoul Karim <savadogoak@gmail.com>
2024-05-11 00:31:56 +0000

    first commit

---
+++ scurve.py
@@ -0,0 +1,34 @@
+import graycurve
+import hcurve
+import hilbert
+import natural
+import zigzag
+import zorder
+
+curveMap = {
+    "hcurve": hcurve.Hcurve,
+    "hilbert": hilbert.Hilbert,
+    "zigzag": zigzag.ZigZag,
+    "zorder": zorder.ZOrder,
+    "natural": natural.Natural,
+    "gray": graycurve.GrayCurve,
+}
+curves = curveMap.keys()
+
+
+def fromSize(curve, dimension, size):
+    """
+        A convenience function for creating a specified curve by specifying
+        size and dimension. All curves implement this common interface.
+    """
+    return curveMap[curve].fromSize(dimension, size)
+
+
+def fromOrder(curve, dimension, order):
+    """
+        A convenience function for creating a specified curve by specifying
+        order and dimension. All curves implement this common interface, but
+        the meaning of "order" may differ for each curve.
+    """
+    return curveMap[curve](dimension, order)
+
